6c06fd1507b11100475fae84dadb87bc9b510279
Umer Dar <umerbashirdar@gmail.com>
2017-04-12 15:52:56 -0500

    Reviewing C Structures

---
+++ Basics/control_structures.rb
@@ -0,0 +1,14 @@
+car1_speed = 500
+car2_speed = 300
+
+# car2_speed += 300
+
+if car1_speed > car2_speed
+	puts "Car 1 is faster than car 2."
+end
+
+car2_speed += 300
+
+if car2_speed > car1_speed
+	puts "Car 2 is faster than car 1."
+end
